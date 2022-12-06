abe62b9f58ceb27fd50480be9b9cc9d2d9090a47
devephy <litephantus@gmail.com>
2022-12-06 11:35:17 +0300

    redux design

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,56 +1,55 @@
-import React, {useState} from 'react';
-import { useSelector, useDispatch } from 'react-redux';
-import './App.css';
-import ReservationsHolder from './components/ReservationsHolder';
-import {addReservation}  from './features/reservation/reservationsSlice';
-
+import React, { useState } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import "./App.css";
+import ReservationsHolder from "./components/ReservationsHolder";
+import { addReservation } from "./features/reservation/reservationsSlice";
 
 function App() {
-  const reservations = useSelector((state)=> state.reservations.value)
- 
-  const [reservationNameInput, setReservationNameInput] = useState("")
-  const dispatch= useDispatch() 
-  const handleAddReservations = () =>{
-    if(!reservationNameInput) return
+  const reservations = useSelector((state) => state.reservations.value);
 
-    dispatch(addReservation(reservationNameInput))
-    setReservationNameInput("")
-    
-  }
+  const [reservationNameInput, setReservationNameInput] = useState("");
+  const dispatch = useDispatch();
+  const handleAddReservations = () => {
+    if (!reservationNameInput) return;
 
-
+    dispatch(addReservation(reservationNameInput));
+    setReservationNameInput("");
+  };
 
   return (
     <div className="App">
-    <div className="container">
-      <div className="reservation-container">
-        <div>
-          <h5 className="reservation-header">Reservations</h5>
-          <div className="reservation-cards-container">
-            {reservations.map(name => {
-              return <ReservationsHolder name={name}/> 
-            })} 
+      <div className="container">
+        <div className="reservation-container">
+          <div>
+            <h5 className="reservation-header">Reservations</h5>
+            <div className="reservation-cards-container">
+              {reservations.map((name) => {
+                return <ReservationsHolder name={name} />;
+              })}
+            </div>
+          </div>
+          <div className="reservation-input-container">
+            <input
+              value={reservationNameInput}
+              onChange={(e) => setReservationNameInput(e.target.value)}
+            />
+            <button onClick={handleAddReservations}>Add</button>
           </div>
         </div>
-        <div className="reservation-input-container">
-          <input value={reservationNameInput} onChange={ (e)=> setReservationNameInput(e.target.value)}/>
-          <button onClick={handleAddReservations}>Add</button>
-        </div>
-      </div>
-      <div className="customer-food-container">
-        <div className="customer-food-card-container">
-          <p>Selena Gomez</p>
-          <div className="customer-foods-container">
-            <div className="customer-food"></div>
-            <div className="customer-food-input-container">
-              <input />
-              <button>Add</button>
+        <div className="customer-food-container">
+          <div className="customer-food-card-container">
+            <p>Data Visual</p>
+            <div className="customer-foods-container">
+              <div className="customer-food"></div>
+              <div className="customer-food-input-container">
+                <input className="send-button" />
+                <button>Add</button>
+              </div>
             </div>
           </div>
         </div>
       </div>
     </div>
-  </div>
   );
 }
 
--- frontend/src/features/reservation/reservationsSlice.js
+++ frontend/src/features/reservation/reservationsSlice.js
@@ -1,20 +1,20 @@
-import {createSlice,} from '@reduxjs/toolkit'
- 
+import { createSlice } from "@reduxjs/toolkit";
+
 const initialState = {
-    value: [],
-}
+  value: [],
+};
 
 export const reservationsSlice = createSlice({
-    name: "reservations",
-    initialState,
-    reducers:{
-        addReservation: (state, action) =>{
-            console.log("Hello");
-            state.value.push(action.payload)
-        },
+  name: "reservations",
+  initialState,
+  reducers: {
+    addReservation: (state, action) => {
+      console.log("Hello");
+      state.value.push(action.payload);
     },
+  },
 });
 
-export const {addReservation} = reservationsSlice.actions
+export const { addReservation } = reservationsSlice.actions;
 
 export default reservationsSlice.reducer;
