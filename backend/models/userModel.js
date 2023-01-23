d74a29ea8bfd839c7a368dce7edd611f28b1efeb
devephy <litephantus@gmail.com>
2023-01-23 09:52:54 +0300

    mods

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -8,7 +8,7 @@ const userSchema = mongoose.Schema({
     },
     email: {
         type: String,
-        required: [true, 'Please an email'],
+        required: [true, 'Please add an email'],
         unique: true
     },
     password: {
